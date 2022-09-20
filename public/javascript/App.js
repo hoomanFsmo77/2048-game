b1541142acf32ab43327fa64dc3a45646b63dba7
hooman_77 <hoomanmousavi@gmail.com>
2022-09-20 18:10:38 +0430

    add touch event for users

--- public/javascript/App.js
+++ public/javascript/App.js
@@ -2,6 +2,10 @@ import {ui} from "./Utilities.js";
 import {moveLeft,moveRight,moveBottom,moveTop} from "./MoveHandler.js";
 import {grid} from "./Grid.js";
 let isStarted=false
+let startX,startY
+let direction
+
+
 
 ui.newGameButton.addEventListener('click',()=>{
     isStarted=true
@@ -34,11 +38,54 @@ window.addEventListener('keydown',e=>{
 })
 
 window.addEventListener('touchstart',e=>{
-    console.log('start')
+    document.body.style.touchAction='none'
+    isStarted && e.preventDefault()
+    startY=e.changedTouches[0].clientY
+    startX=e.changedTouches[0].clientX
 })
 window.addEventListener('touchmove',e=>{
-    console.log('move')
+    document.body.style.touchAction='none'
+    isStarted && e.preventDefault()
 })
 window.addEventListener('touchend',e=>{
-    console.log('end')
+    document.body.style.touchAction='none'
+    isStarted && e.preventDefault()
+
+    let endX=e.changedTouches[0].clientX
+    let endY=e.changedTouches[0].clientY
+
+    let dx=endX-startX
+    let dy=endY-startY
+
+    let absDx=Math.abs(dx)
+    let absDy=Math.abs(dy)
+
+    if (Math.max(absDx, absDy) > 10) {
+        let direction=absDx > absDy ? (dx > 0 ? 'right' : 'left') : (dy > 0 ? 'down' : 'up')
+        switch (direction) {
+            case 'right':{
+                isStarted && moveRight()
+            }
+                break
+            case 'down':{
+                isStarted && moveBottom()
+            }
+                break
+            case 'up':{
+                isStarted && moveTop()
+            }
+                break
+            case 'left':{
+                isStarted && moveLeft()
+            }
+            break
+        }
+        console.log(direction)
+    }
+
+
+    // direction= lastY > e.changedTouches[0].clientY + 5 ?  'Down'  :  'Up'
+    // direction=lastX > e.changedTouches[0].clientX+5 ? 'right' : 'left'
+    //
+    // console.log(direction)
 })
